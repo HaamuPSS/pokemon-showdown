1d276725c89c20c2e27fa2bb10f051af0c6ec5ab
HaamuPSS <84972052+HaamuPSS@users.noreply.github.com>
2022-10-14 16:01:18 +0300

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -4623,10 +4623,6 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			if (target !== source && move.category !== 'Status') {
 				const yourItem = target.takeItem(source);
 				if (!yourItem) return;
-				if (!source.setItem(yourItem)) {
-					target.item = yourItem.id;
-					return;
-				}
 				this.add('-item', source, yourItem, '[from] ability: Rats Gambit', '[of] ' + source);
 			}
 		},
