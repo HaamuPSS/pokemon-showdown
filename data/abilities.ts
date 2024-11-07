6600d2870867f4b21e52e30b39b0405a34e62a82
HaamuPSS <84972052+HaamuPSS@users.noreply.github.com>
2024-11-07 13:51:02 +0200

    Treasure hunter fix

--- data/abilities.ts
+++ data/abilities.ts
@@ -5796,7 +5796,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			const target = this.sample(possibleTargets);
 			const item = target.getItem();
 			if (pokemon.setItem(item)) {
-				this.add('-ability', pokemon, ability, '[from] ability: Treasure Hunter', '[of] ' + target);
+				this.add('-item', pokemon, item, '[from] ability: Treasure Hunter', '[of] ' + target);
 			}
 		},
 		flags: {failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1},
